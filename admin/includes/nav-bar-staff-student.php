6117af0d0e713188f5e7d84f805a9bd221e6f78a
ajith kumar G <you@example.com>
2019-11-10 11:44:42 +0530

    ajith nav bar edited

--- admin/includes/nav-bar-staff-student.php
+++ admin/includes/nav-bar-staff-student.php
@@ -5,13 +5,21 @@
   </button> 
   <div class="collapse navbar-collapse" id="collapsibleNavbar">
     <ul class="navbar-nav ml-auto">
-      
+     <?php if($_SESSION['role'] == 'Staff') {?>
       <li class="nav-item">
         <a class="nav-link bg-danger text-white" href="genrep.php?deptid=<?php if(isset($_GET['deptid'])) echo $_GET['deptid']; ?>">REPORT</a>
       </li>
       <li class="nav-item">
         <a class="nav-link text-white bg-primary" href="sessionclose.php">LOGOUT</a>
       </li> 
+    <?php } 
+    elseif($_SESSION['role'] == 'Student'){?>
+
+<li class="nav-item">
+        <a class="nav-link text-white bg-primary" href="sessionclose.php">LOGOUT</a>
+      </li> 
+    <?php }?>
+
     </ul>
   </div> 
 </nav>
